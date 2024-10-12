254ac12dc781b6a43443a68789b2fbd911c8b499
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2024-10-12 16:09:38 +0200

    Add files via upload

---
+++ Config.example.h
@@ -0,0 +1,3 @@
+const char *ssid = "YOUR SSID";
+const char *wifiPassword = "YOUR PASSWORD";
+const char *botToken = "YOUR BOT TOKEN";
---
+++ TelegramBot.ino
@@ -0,0 +1,48 @@
+#include "Config.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+
+WiFiClientSecure client;
+
+UniversalTelegramBot bot(botToken, client);
+
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+void handleCommand(String text, String chat_id) {
+  if (text == "/start") {
+    bot.sendMessage(chat_id, "Hiii", "");
+  } else {
+    bot.sendMessage(chat_id, "Comando no reconocido", "");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, wifiPassword);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to: " + String(ssid));
+  }
+  Serial.println("Connected to: " + String(ssid));
+  client.setInsecure();
+}
+
+void loop() {
+  if (millis() - lastTimeBotRan > botRequestDelay) {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages) {
+      for (int i = 0; i < numNewMessages; i++) {
+        String chat_id = String(bot.messages[i].chat_id);
+        String text = bot.messages[i].text;
+        Serial.println("Mensaje recibido: " + text);
+        handleCommand(text, chat_id);
+      }
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+}
